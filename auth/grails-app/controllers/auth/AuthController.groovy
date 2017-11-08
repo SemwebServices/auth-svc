87d3adcb115a1e6dab908a44450f05f46a591d7c
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-11-08 10:42:11 +0000

    Working on auth controller

--- auth/grails-app/controllers/auth/AuthController.groovy
+++ auth/grails-app/controllers/auth/AuthController.groovy
@@ -17,6 +17,19 @@ class AuthController {
   @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
   def index() { 
     def result = [:]
+
+    if ( ( params.provider ) && ( grailsApplication.config.authProviders[params.provider] ) {
+      def oidc_cfg = grailsApplication.config.authProviders[params.provider]
+      log.debug("Processing auth request for provider ${params.provider} : ${oidc_cfg}");
+      def state=java.util.UUID.randomUUID();
+      def nonce=java.util.UUID.randomUUID();
+      redirect(url:"${oidc_cfg.authorization_endpoint}?client_id=${oidc_cfg.clientId}"+
+                            '&response_type=code'+
+                            '&scope=openid%20email'+
+                            '&redirect_uri=http://localhost:8080/oidcCallback'+
+                            "&state=${state}"+
+                            "&nonce=${nonce}")
+    }
     log.debug("AuthController::index(${params})");
     if ( params.redirect ) {
       log.debug("Redirecting to ${params.redirect}");
